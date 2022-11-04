3457e236f8613aeb64a3cdba3589c2686ce6fdba
dragonnomada <dragonnomada123@gmail.com>
2022-11-04 11:58:25 -0600

    v1.0.0 r1

---
+++ index.js
@@ -0,0 +1,17 @@
+const { load } = require("./lib")
+
+async function main() {
+    const world = await load("drafts/world.yaml")
+    console.log(world)
+    world.describe()
+    
+    const player = await load("drafts/player.yaml")
+    console.log(player)
+    player.describe()
+    
+    const player_controller = await load("drafts/player_controller.yaml")
+    console.log(player_controller)
+    player_controller.describe()
+}
+
+main().catch(error => console.log(`${error}`))
