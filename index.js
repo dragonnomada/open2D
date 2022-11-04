96e2dd97021866272a041aca3af303dd59b5067d
dragonnomada <dragonnomada123@gmail.com>
2022-11-04 12:40:29 -0600

    v1.0.0 r4

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const load = require("./lib/loader")
 
 async function main() {
     const scene = await load("drafts/scene.yaml")
-    scene.log()
     scene.describe()
+    
+    await scene.create()
+    scene.log()
+
+    await scene.run()
 }
 
 main().catch(error => console.log(`${error}`))
