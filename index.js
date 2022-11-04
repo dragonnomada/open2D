c8d0f4452117329c9595cdaddb965ac8d82b4d7b
dragonnomada <dragonnomada123@gmail.com>
2022-11-04 12:27:05 -0600

    v1.0.0 r3

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 const load = require("./lib/loader")
 
 async function main() {
-    const world = await load("drafts/world.yaml")
-    console.log(world)
-    world.describe()
-    
-    const player = await load("drafts/player.yaml")
-    console.log(player)
-    player.describe()
-    
-    const player_controller = await load("drafts/player_controller.yaml")
-    console.log(player_controller)
-    player_controller.describe()
+    const scene = await load("drafts/scene.yaml")
+    scene.log()
+    scene.describe()
 }
 
 main().catch(error => console.log(`${error}`))
